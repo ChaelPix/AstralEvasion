c35d6c28e7562b40de11654e0b875cebe181ec81
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2024-03-28 20:45:05 +0100

    bdd

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
+using MySql.Data.MySqlClient;
+using System;
 
 public class LeaderboardManager : MonoBehaviour
 {
+    [SerializeField] private Transform leaderboardGrid;
+    [SerializeField] private TextMeshProUGUI leaderboardPrfb;
+    [SerializeField] private Color32[] colors;
+
     private string playerName;
     private int score;
-    private readonly string addScoreURL = "http://dreamlo.com/lb/YEjT9zaMT0eZpQuBfq1utgEMW89ATqGkeTTfe6ZIXaxQ/add/{0}/{1}";
-    private readonly string leaderboardURL = "http://dreamlo.com/lb/66046c508f40bb8118033f4f/json-asc";
-
-    [SerializeField] Transform leaderboardGrid;
-    [SerializeField] TextMeshProUGUI leaderboardPrfb;
-    [SerializeField] TextMeshProUGUI leaderboardScorePrfb;
-    [SerializeField] Color32[] colors;
+    private string connStr = "server=ky2.h.filess.io;user=games_enterdeer;database=games_enterdeer;port=3306;password=bd44e7b4ef8b8b7977e391dcd6bf009dc7e0be83";
 
     private void Awake()
     {
-        GetLeaderboard();
+        StartCoroutine(GetLeaderboard());
     }
 
     public void Init(string _name)
@@ -26,102 +25,74 @@ public class LeaderboardManager : MonoBehaviour
         playerName = _name;
     }
 
-    public void EndGame(int score)
+    public void EndGame(int _score)
     {
-        this.score = score;
+        score = _score;
         StartCoroutine(UploadScore(playerName, score));
     }
 
-    IEnumerator UploadScore(string playerName, int score)
+    IEnumerator UploadScore(string _playerName, int _score)
     {
-        string url = string.Format(addScoreURL, WWW.EscapeURL(playerName), score);
-        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        using (MySqlConnection conn = new MySqlConnection(connStr))
         {
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success)
+            try
             {
-                Debug.Log(www.error);
+                conn.Open();
+                string sql = "INSERT INTO supernova_players (player_name, score) VALUES (@playerName, @score)";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@playerName", _playerName);
+                cmd.Parameters.AddWithValue("@score", _score);
+                cmd.ExecuteNonQuery();
             }
-            else
+            catch (System.Exception ex)
             {
-                Debug.Log("Score upload successful");
-            }
-        }
-    }
-
-    public void GetLeaderboard()
-    {
-        StartCoroutine(DownloadLeaderboard());
-    }
-
-    IEnumerator DownloadLeaderboard()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get(leaderboardURL))
-        {
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                //Debug.Log(www.downloadHandler.text);
-                ProcessLeaderboardData(www.downloadHandler.text);
+                Debug.LogError(ex.ToString());
             }
         }
+        yield return null;
     }
 
-    public void ProcessLeaderboardData(string json)
+    IEnumerator GetLeaderboard()
     {
-        LeaderboardRoot leaderboardData = JsonUtility.FromJson<LeaderboardRoot>(json);
-        List<LeaderboardEntry> entries = new List<LeaderboardEntry>(leaderboardData.dreamlo.leaderboard.entry);
-        entries.Sort((entry1, entry2) => entry2.score.CompareTo(entry1.score));
-
-        foreach (var entry in entries)
+        List<LeaderboardEntry> entries = new List<LeaderboardEntry>();
+        using (MySqlConnection conn = new MySqlConnection(connStr))
         {
-            Debug.Log($"Name: {entry.name}, Score: {entry.score}");
+            try
+            {
+                conn.Open();
+                string sql = "SELECT player_name, score FROM supernova_players ORDER BY score DESC LIMIT 6";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        entries.Add(new LeaderboardEntry { name = rdr["player_name"].ToString(), score = Convert.ToInt32(rdr["score"]) });
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex.ToString());
+            }
         }
 
-        for (int i = 0; i < entries.Count; i++) 
+        for (int i = 0; i < entries.Count; i++)
         {
-            if (i >= 6)
-                break;
-
             TextMeshProUGUI l = Instantiate(leaderboardPrfb, leaderboardGrid);
-            l.SetText("#" + (i + 1) + " " + entries[i].name.Replace("+", " "));
+            l.text = "#" + (i + 1) + " " + entries[i].name.Replace("+", " ");
+            l.color = colors[i % colors.Length];
 
             TextMeshProUGUI ll = Instantiate(leaderboardPrfb, leaderboardGrid);
-            ll.SetText("| " + entries[i].score.ToString());
-
-            l.color = colors[i];
-            ll.color = colors[i];
+            ll.text = "| " + entries[i].score.ToString();
+            ll.color = colors[i % colors.Length];
         }
+
+        yield return null;
     }
 }
-
 [System.Serializable]
 public class LeaderboardEntry
 {
     public string name;
-    public int score; 
-}
-
-[System.Serializable]
-public class Leaderboard
-{
-    public LeaderboardEntry[] entry;
-}
-
-[System.Serializable]
-public class Dreamlo
-{
-    public Leaderboard leaderboard;
-}
-
-[System.Serializable]
-public class LeaderboardRoot
-{
-    public Dreamlo dreamlo;
+    public int score;
 }
